9228acc43688480ebc02407e4dd3e310f6b23dd8
zackquixote <lourdianrentinoakol@gmail.com>
2026-04-27 12:56:14 +0800

    feat(model): add CertificateModel

---
+++ app/Models/CertificateModel.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CertificateModel extends Model
+{
+    protected $table      = 'certificates';
+    protected $primaryKey = 'id';
+    
+    // Strictly matching your simplified SQL columns
+    protected $allowedFields = [
+        'resident_id', 
+        'certificate_type', 
+        'purpose', 
+        'created_by',
+        'created_at'
+    ];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = ''; // No updated_at column in simplified SQL
+
+    /**
+     * Get all certificates with Resident Name and Creator Name (For Recent Activity)
+     */
+    public function getRecentActivity($limit = 10)
+    {
+        return $this->select('certificates.*, 
+                             CONCAT(residents.first_name, " ", residents.last_name) as resident_name,
+                             users.username as created_by_name')
+                    ->join('residents', 'residents.id = certificates.resident_id')
+                    ->join('users', 'users.id = certificates.created_by', 'left') // Left join in case user is deleted
+                    ->orderBy('certificates.created_at', 'DESC')
+                    ->limit($limit)
+                    ->findAll();
+    }
+
+    /**
+     * Get specific certificate details for printing
+     */
+    public function getCertificateForPrint($id)
+    {
+        return $this->select('certificates.*, residents.*, certificates.id as cert_id')
+                    ->join('residents', 'residents.id = certificates.resident_id')
+                    ->where('certificates.id', $id)
+                    ->first();
+    }
+
+    /**
+     * Helper to get Barangay Settings (Captain Name, etc.)
+     * TODO: Move this to a SettingsModel later
+     */
+    public function getBarangaySettings()
+    {
+        return (object)[
+            'barangay_name' => 'SAN ISIDRO',
+            'municipality'  => 'SAMPLE TOWN',
+            'province'      => 'PROVINCE OF SAMPLE',
+            'captain_name'  => 'HON. JUAN DELA CRUZ',
+        ];
+    }
+}
